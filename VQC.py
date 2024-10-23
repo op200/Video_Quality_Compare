aae11273990cb7097cee8c7eb5ca4ea9ddd1003f
op200 <112297583+op200@users.noreply.github.com>
2024-10-23 15:07:13 +0800

    v1.0

--- VQC.py
+++ VQC.py
@@ -5,9 +5,10 @@ import cv2
 from PIL import Image, ImageTk
 import numpy as np
 
+# import matplotlib
+# matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
-from matplotlib.widgets import Cursor
 import mplcursors
 
 from threading import Thread
@@ -24,7 +25,7 @@ import re
 from time import sleep
 
 PROGRAM_NAME = "Video Quality Compare"
-VERSION = "0.1"
+VERSION = "1.0"
 HOME_LINK = "https://github.com/op200/Video_Quality_Compare"
 
 #日志
@@ -89,7 +90,7 @@ def save_config():
 
 
 input_video_path_1, input_video_path_2, input_video_path_now = str, str, str
-scale, frame_height, frame_width, new_frame_height, new_frame_width = False, int,int,int,int
+scale, cut_scale, frame_height, frame_width, new_frame_height, new_frame_width = False, False, int,int,int,int
 right_x,right_y,left_x,left_y = 0,0,0,0
 fps:int
 
@@ -99,6 +100,7 @@ VIDEO_FRAME_IMG_HEIGHT = 6
 
 root_Tk = tk.Tk()
 root_Tk.title(PROGRAM_NAME)
+root_Tk.resizable(False, False)
 def root_Tk_Close():
     global is_listener_data
     is_listener_data = False
@@ -147,7 +149,7 @@ def create_help_about_Toplevel():
     frame.pack(expand=True)
     ttk.Label(frame,text=f"{PROGRAM_NAME}  v{VERSION}\n\n").pack()
 
-    hyperlink_Label = ttk.Label(frame,text=HOME_LINK, cursor="hand2", foreground="blue", font=underline_font)
+    hyperlink_Label = ttk.Label(frame,text=HOME_LINK, cursor="hand2", foreground='blue', font=underline_font)
     hyperlink_Label.bind("<Button-1>",lambda _:hyperlink_jump(HOME_LINK))
     hyperlink_Label.pack()
     
@@ -169,7 +171,7 @@ right_Frame.grid(row=0,column=1,padx=5,pady=5)
 #左侧控件
 
 #视频预览控件
-video_review_Label = ttk.Label(left_Frame, cursor="target")
+video_review_Label = ttk.Label(left_Frame)
 
 #进度条控件
 video_Progressbar = ttk.Progressbar(left_Frame)
@@ -198,20 +200,25 @@ video_frame_Label = ttk.Label(left_Frame)
 frame_count = 0
 frame_now = 0
 
+cut_scale_x = 0
+cut_scale_y = 0
+
 #跳转当前帧
 def jump_to_frame():
     global scale,frame_now,frame_count
     main_rendering_Cap.set(cv2.CAP_PROP_POS_FRAMES,frame_now)
-    _, frame = main_rendering_Cap.read()
+    frame = main_rendering_Cap.read()[1]
     try:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     except cv2.error:
         log.warning(f"[{frame_now}]该帧无法读取(应检查视频封装)")
     else:
         if scale:
-            frame = cv2.resize(frame,(new_frame_width,new_frame_height))
+            if cut_scale:
+                frame = frame[cut_scale_y : cut_scale_y+new_frame_height, cut_scale_x : cut_scale_x+new_frame_width]
+            else:
+                frame = cv2.resize(frame,(new_frame_width,new_frame_height))
 
-        
         video_Progressbar["value"] = frame_now/(frame_count-1)*100
         
         photo = ImageTk.PhotoImage(Image.fromarray(frame))
@@ -260,6 +267,7 @@ def submit_path(_):
     #渲染控件
     frame_num_Frame.grid(row=2,column=0)
     change_video_preview_Button.grid(row=5,column=0,pady=8)
+    change_video_scale_Button.grid(row=6,column=0)
     cmp_set_Frame.grid(row=7,column=0,pady=5)
     log_Frame.grid(row=8,column=0,pady=8)
 
@@ -280,6 +288,7 @@ def submit_path(_):
             new_frame_width,new_frame_height = int(frame_width/scale),int(frame_height/scale)
             frame = cv2.resize(frame,(new_frame_width,new_frame_height))
             log.info(f"视频画幅过大 预览画面已缩小(1/{scale:.2f}-->{new_frame_width}x{new_frame_height})")
+            change_video_scale_Button.config(state=tk.NORMAL)
         else:
             new_frame_width,new_frame_height = frame_width,frame_height
             scale = False
@@ -296,7 +305,6 @@ def submit_path(_):
 
         #初始化右侧控件
         frame_count = int(main_rendering_Cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        algorithm.data = [None]*frame_count
         frame_count_Label.config(text = f" / {frame_count-1:9d}")
         frame_now_Tkint.set(0)
         def _long_text_cut(text):
@@ -367,6 +375,44 @@ def change_video_preview():
     jump_to_frame()
 change_video_preview_Button = ttk.Button(input_video_Frame, text="切换预览视频", width=12, command=change_video_preview)
 
+video_review_on_drag_start_x:int
+video_review_on_drag_start_y:int
+cut_scale_x_start:int
+cut_scale_y_start:int
+def video_review_on_drag_start(event):
+    global video_review_on_drag_start_x,video_review_on_drag_start_y,cut_scale_x_start,cut_scale_y_start
+    video_review_on_drag_start_x = event.x
+    video_review_on_drag_start_y = event.y
+    cut_scale_x_start = cut_scale_x
+    cut_scale_y_start = cut_scale_y
+def video_review_on_drag(event):
+    global cut_scale_x,cut_scale_y
+    cut_scale_x = cut_scale_x_start - event.x + video_review_on_drag_start_x
+    cut_scale_y = cut_scale_y_start - event.y + video_review_on_drag_start_y
+    if cut_scale_x + new_frame_width > frame_width:
+        cut_scale_x = frame_width - new_frame_width
+    if cut_scale_x < 0:
+        cut_scale_x = 0
+    if cut_scale_y + new_frame_height > frame_height:
+        cut_scale_y = frame_height - new_frame_height
+    if cut_scale_y < 0:
+        cut_scale_y = 0
+    jump_to_frame()
+
+def change_video_scale():
+    global cut_scale
+    if cut_scale:
+        video_review_Label.unbind("<Button-1>")
+        video_review_Label.unbind("<B1-Motion>")
+        cut_scale = False
+    else:
+        video_review_Label.bind("<Button-1>", video_review_on_drag_start)
+        video_review_Label.bind("<B1-Motion>", video_review_on_drag)
+        cut_scale = True
+    jump_to_frame()
+change_video_scale_Button = ttk.Button(input_video_Frame, text="切换缩放", width=8, command=change_video_scale)
+change_video_scale_Button.config(state=tk.DISABLED)
+
 #帧数
 frame_num_Frame = ttk.Frame(right_Frame)
 
@@ -405,7 +451,7 @@ video_fps_Label = ttk.Label(video_info_Frame)
 
 cmp_set_Frame = ttk.Frame(right_Frame)
 
-cmp_select_Combobox = ttk.Combobox(cmp_set_Frame, width=4, values=['ssim','psnr','vmaf'])
+cmp_select_Combobox = ttk.Combobox(cmp_set_Frame, width=4, values=['ssim','psnr','vmaf','mse'])
 cmp_select_Combobox.state(['readonly'])
 cmp_select_Combobox.set(config.get("DEFAULT","cmp_type"))
 cmp_select_Combobox.grid(row=0,column=0)
@@ -413,18 +459,22 @@ cmp_select_Combobox.grid(row=0,column=0)
 start_cmp_Frame = tk.Frame(cmp_set_Frame)
 start_cmp_Frame.grid(row=1,column=0,pady=15)
 
-max_min_Frame = ttk.Frame(start_cmp_Frame)
-max_min_Frame.grid(row=0,column=0)
+extreme_value_Frame = ttk.Frame(start_cmp_Frame)
+extreme_value_Frame.grid(row=0,column=0)
 
-max_data_Label = ttk.Label(max_min_Frame,width=22,anchor='center')
+max_data_Label = ttk.Label(extreme_value_Frame,width=14,anchor='center',foreground='#00BB00')
 max_data_Label.grid(row=0,column=0)
 max_data_Label.config(state=tk.DISABLED)
 
-min_data_Label = ttk.Label(max_min_Frame,width=22,anchor='center')
-min_data_Label.grid(row=0,column=1)
+avg_data_Label = ttk.Label(extreme_value_Frame,width=14,anchor='center',foreground='#BBBB00')
+avg_data_Label.grid(row=0,column=1)
+avg_data_Label.config(state=tk.DISABLED)
+
+min_data_Label = ttk.Label(extreme_value_Frame,width=14,anchor='center',foreground='#BB0000')
+min_data_Label.grid(row=0,column=2)
 min_data_Label.config(state=tk.DISABLED)
 
-show_data_Label = ttk.Label(start_cmp_Frame,width=44,anchor='center')
+show_data_Label = ttk.Label(start_cmp_Frame,width=42,anchor='center')
 show_data_Label.grid(row=1,column=0,pady=5)
 
 start_cmp_Button = ttk.Button(start_cmp_Frame,text="开始")
@@ -447,7 +497,7 @@ def show_plt():
     def on_add(sel):
         # 文本样式
         x, y = sel.target
-        sel.annotation.set_text(f"{sel.artist.get_label()}\nX: {x}\nY: {y}")
+        sel.annotation.set_text(f"{'frame num:':<12}{str(round(x,6)):>9}\n{cmp_select_Combobox.get()+' '+sel.artist.get_label()+':':<12}{str(round(y,6)):>9}")
         sel.annotation.set_fontsize(12)
         sel.annotation.set_color((0.49, 0.1216, 0.682))
         sel.annotation.get_bbox_patch().set_facecolor((0.8, 0.8, 0.8))
@@ -475,77 +525,86 @@ log_vScrollbar.config(command=log_Text.yview)
 class algorithm:
     algorithm:str
     filter_option:str
-    data = [None]
+    data:list
     max_data:float
     min_data:float
+    avg_data:float
+    is_encoding = False
 
     @staticmethod
     def find_line_ssim(line):
-        return re.findall("[\d\.]+", re.search("Y(.*)\(",line).group(1))
+        return re.findall(r"[\d\.]+", re.search(r"Y(.*)\(",line).group(1))
 
     @staticmethod
     def find_line_psnr(line):
-        return re.findall("[\d\.]+", re.search("psnr_avg(.+)$",line).group(1))
+        return re.findall(r"[\d\.]+", re.search(r"psnr_avg(.+)$",line).group(1))
+
+    @staticmethod
+    def find_line_mse(line):
+        return re.findall(r"[\d\.]+", re.search(r"mse_avg(.+)psnr_avg",line).group(1))
     
     find_line = find_line_ssim
 
 
     @staticmethod
-    def read_file_ssim_psnr(file):
+    def read_file_ssim_psnr_mse(file):
         for line in file:
             algorithm.data.append([float(v) for v in algorithm.find_line(line)])
 
     @staticmethod
     def read_file_vmaf(file):
         for line in file:
-            text = re.search('vmaf="([\d\.]+)"', line)
+            text = re.search(r'vmaf="([\d\.]+)"', line)
             if text:
                 algorithm.data.append(float(text.group(1)))
             else:
-                text = re.search('<metric name="vmaf" min="([\d\.]+)" max="([\d\.]+)"', line)
+                text = re.search(r'<metric name="vmaf" min="([\d\.]+)" max="([\d\.]+)" mean="([\d\.]+)"', line)
                 if text:
                     algorithm.min_data = float(text.group(1))
                     algorithm.max_data = float(text.group(2))
-        
+                    algorithm.avg_data = float(text.group(3))
 
-    read_file = read_file_ssim_psnr
+    read_file = read_file_ssim_psnr_mse
 
 
     @staticmethod
     def get_data_ssim():
         algorithm.min_data = algorithm.data[0][3]
         algorithm.max_data = algorithm.data[0][3]
+        algorithm.avg_data = 0
         for v in algorithm.data:
+            algorithm.avg_data += v[3]
             if v[3] > algorithm.max_data:
                 algorithm.max_data = v[3]
             if v[3] < algorithm.min_data:
                 algorithm.min_data = v[3]
+        algorithm.avg_data = round(algorithm.avg_data/frame_count,6)
 
     @staticmethod
-    def get_data_psnr():
+    def get_data_psnr_mse():
         algorithm.min_data = algorithm.data[0][0]
         algorithm.max_data = algorithm.data[0][0]
+        algorithm.avg_data = 0
         for v in algorithm.data:
+            algorithm.avg_data += v[0]
             if v[0] > algorithm.max_data:
                 algorithm.max_data = v[0]
             if v[0] < algorithm.min_data:
                 algorithm.min_data = v[0]
+        algorithm.avg_data = round(algorithm.avg_data/frame_count,2)
 
     @staticmethod
     def get_data_vmaf():
-        algorithm.min_data = algorithm.data[0]
-        algorithm.max_data = algorithm.data[0]
-        for v in algorithm.data:
-            if v > algorithm.max_data:
-                algorithm.max_data = v
-            if v < algorithm.min_data:
-                algorithm.min_data = v
+        pass
 
     get_data = get_data_ssim
 
     @staticmethod
     def listen_data_ssim():
-        v = algorithm.data[frame_now]
+        try:
+            v = algorithm.data[frame_now]
+        except:
+            return
         if v:
             dif = algorithm.max_data-algorithm.min_data
             if dif==0:
@@ -554,10 +613,13 @@ class algorithm:
                 color_percent = (float(v[3])-algorithm.min_data)/dif
             show_data_Label.config(
                 text=f"Y:{v[0]}  U:{v[1]}  V:{v[2]}  All:{v[3]}",
-                foreground=f"#{round(255*(1-color_percent)):02X}{round(255*color_percent):02X}00")
+                foreground=f"#{round(187 if color_percent <= 0.5 else 374*(1-color_percent)):02X}{round(187 if color_percent >= 0.5 else 374*color_percent):02X}00")
 
-    def listen_data_psnr():
-        v = algorithm.data[frame_now]
+    def listen_data_psnr_mse():
+        try:
+            v = algorithm.data[frame_now]
+        except:
+            return
         if v:
             dif = algorithm.max_data-algorithm.min_data
             if dif==0:
@@ -566,10 +628,13 @@ class algorithm:
                 color_percent = (float(v[0])-algorithm.min_data)/dif
             show_data_Label.config(
                 text=f"Y:{v[1]}  U:{v[2]}  V:{v[3]}  All:{v[0]}",
-                foreground=f"#{round(255*(1-color_percent)):02X}{round(255*color_percent):02X}00")
+                foreground=f"#{round(187 if color_percent <= 0.5 else 374*(1-color_percent)):02X}{round(187 if color_percent >= 0.5 else 374*color_percent):02X}00")
 
     def listen_data_vmaf():
-        v = algorithm.data[frame_now]
+        try:
+            v = algorithm.data[frame_now]
+        except:
+            return
         if v:
             dif = algorithm.max_data-algorithm.min_data
             if dif==0:
@@ -578,7 +643,7 @@ class algorithm:
                 color_percent = (float(v)-algorithm.min_data)/dif
             show_data_Label.config(
                 text=f"Score:{v}",
-                foreground=f"#{round(255*(1-color_percent)):02X}{round(255*color_percent):02X}00")
+                foreground=f"#{round(187 if color_percent <= 0.5 else 374*(1-color_percent)):02X}{round(187 if color_percent >= 0.5 else 374*color_percent):02X}00")
 
     listen_data = listen_data_ssim
 
@@ -598,7 +663,11 @@ class algorithm:
         y = [i[3] for i in algorithm.data]
         plt.plot(x, y, color='black', label="All")
 
-    def get_plt_psnr():
+        plt.axhline(y=algorithm.max_data, linestyle='--', label="Max", color='#00BB00')
+        plt.axhline(y=algorithm.avg_data, linestyle='--', label="Avg", color='#BBBB00')
+        plt.axhline(y=algorithm.min_data, linestyle='--', label="Min", color='#BB0000')
+
+    def get_plt_psnr_mse():
         x = [i for i in range(0,len(algorithm.data))]
 
         y = [i[1] for i in algorithm.data]
@@ -613,11 +682,19 @@ class algorithm:
         y = [i[0] for i in algorithm.data]
         plt.plot(x, y, color='black', label="All")
 
+        plt.axhline(y=algorithm.max_data, linestyle='--', label="Max", color='#00BB00')
+        plt.axhline(y=algorithm.avg_data, linestyle='--', label="Avg", color='#BBBB00')
+        plt.axhline(y=algorithm.min_data, linestyle='--', label="Min", color='#BB0000')
+
     def get_plt_vmaf():
         x = [i for i in range(0,len(algorithm.data))]
 
         y = [i for i in algorithm.data]
         plt.plot(x, y, color='black', label="Score")
+
+        plt.axhline(y=algorithm.max_data, linestyle='--', label="Max", color='#00BB00')
+        plt.axhline(y=algorithm.avg_data, linestyle='--', label="Avg", color='#BBBB00')
+        plt.axhline(y=algorithm.min_data, linestyle='--', label="Min", color='#BB0000')
         
 
     get_plt = get_plt_ssim
@@ -629,24 +706,30 @@ class algorithm:
             case "ssim":
                 algorithm.find_line = algorithm.find_line_ssim
                 algorithm.filter_option = "ssim=f="
-                algorithm.read_file = algorithm.read_file_ssim_psnr
+                algorithm.read_file = algorithm.read_file_ssim_psnr_mse
                 algorithm.get_data = algorithm.get_data_ssim
                 algorithm.listen_data = algorithm.listen_data_ssim
                 algorithm.get_plt = algorithm.get_plt_ssim
             case "psnr":
                 algorithm.find_line = algorithm.find_line_psnr
                 algorithm.filter_option = "psnr=f="
-                algorithm.read_file = algorithm.read_file_ssim_psnr
-                algorithm.get_data = algorithm.get_data_psnr
-                algorithm.listen_data = algorithm.listen_data_psnr
-                algorithm.get_plt = algorithm.get_plt_psnr
+                algorithm.read_file = algorithm.read_file_ssim_psnr_mse
+                algorithm.get_data = algorithm.get_data_psnr_mse
+                algorithm.listen_data = algorithm.listen_data_psnr_mse
+                algorithm.get_plt = algorithm.get_plt_psnr_mse
             case "vmaf":
                 algorithm.filter_option = "libvmaf=log_fmt=xml:log_path="
                 algorithm.read_file = algorithm.read_file_vmaf
                 algorithm.get_data = algorithm.get_data_vmaf
                 algorithm.listen_data = algorithm.listen_data_vmaf
                 algorithm.get_plt = algorithm.get_plt_vmaf
-
+            case "mse":
+                algorithm.find_line = algorithm.find_line_mse
+                algorithm.filter_option = "psnr=f="
+                algorithm.read_file = algorithm.read_file_ssim_psnr_mse
+                algorithm.get_data = algorithm.get_data_psnr_mse
+                algorithm.listen_data = algorithm.listen_data_psnr_mse
+                algorithm.get_plt = algorithm.get_plt_psnr_mse
 
 
 def start_to_ready():
@@ -713,12 +796,24 @@ start_cmp_Button.config(command=start_to_ready)
 def Listener_data():
     global is_listener_data
     while is_listener_data:
-        algorithm.listen_data()
+        if not algorithm.is_encoding:
+            algorithm.listen_data()
         sleep(0.2)
 Thread(target=Listener_data).start()
 
+def flush_ffmpeg_speed_progress(frame):
+    global frame_now
+    frame-=1
+    if frame >= frame_count:
+        frame_now = frame_count-1
+        log.warning(f"ffmpeg 解码的视频帧数({frame})与 cv2 解码的帧数({frame_count})不一致")
+    else:
+        frame_now = frame
+    jump_to_frame()
+
 def Thread_encoding():
     global process_ffmpeg
+    algorithm.is_encoding = True
     log.info("开始")
 
     algorithm.change_algorithm()
@@ -729,8 +824,17 @@ def Thread_encoding():
         '-i', str(input_video_path_2),
         '-lavfi',algorithm.filter_option+config_dir.replace("C:","",1).replace('\\',"/")+"/output.log",
         '-f','null','-'
-    ])
+    ],stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
 
+    while True:
+        line = process_ffmpeg.stdout.readline()
+        if not line and process_ffmpeg.poll() is not None:
+            break
+        if line:
+            print(f"ffmpeg out: {line}", end='')
+            frame = re.search(r"frame=\s*(\d+)\s",line)
+            if frame:
+                flush_ffmpeg_speed_progress(int(frame.group(1)))
     process_ffmpeg.wait()
 
     algorithm.data=[]
@@ -739,12 +843,12 @@ def Thread_encoding():
 
     algorithm.get_data()
 
-    max_data_Label.config(state=tk.NORMAL,
-                          text=f"Max:{algorithm.max_data}", foreground='green')
-    min_data_Label.config(state=tk.NORMAL,
-                          text=f"Min:{algorithm.min_data}", foreground='red')
+    max_data_Label.config(state=tk.NORMAL,text=f"Max:{algorithm.max_data}")
+    avg_data_Label.config(state=tk.NORMAL,text=f"Avg:{algorithm.avg_data}")
+    min_data_Label.config(state=tk.NORMAL,text=f"Min:{algorithm.min_data}")
 
     log.info("完成")
+    algorithm.is_encoding = False
     end_to_ready()
 
 
